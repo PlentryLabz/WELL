1c9bf57476b354145d4bb79e04c43c318fc938db
Shumilkin Gena <shumilkin.gena@yandex.ru>
2014-08-03 17:27:16 +0400

    [API] Users CRUD.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Thelist::Application.routes.draw do
   resource :wellcome, only: [:show]
   root to: 'wellcome#show'
 
-  resources :users, only: [:index, :show, :edit]
-
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :lists, except: [:new, :edit] do
@@ -12,7 +10,7 @@ Thelist::Application.routes.draw do
           resources :records, except: [:new, :edit]
         end
       end
-      resources :users, only: [:index, :show, :update]
+      resources :users, except: [:new, :edit]
 
     end
   end
